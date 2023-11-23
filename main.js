27cc76af1ac15458e98919a75fb3773180ca99a0
Zojo24 <150096317+Zojo24@users.noreply.github.com>
2023-11-23 22:40:56 +0900

    Add files via upload
    cloning MUJI website

---
+++ main.js
@@ -0,0 +1,51 @@
+//swiper//
+new Swiper('.main__visual .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 4000 
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+new Swiper('.main__new .swiper', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+new Swiper('.main__mdpick .swiper', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+new Swiper('.main__season .swiper', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+new Swiper('.main__furniture .swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
